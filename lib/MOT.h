3c0ff6e813fea15eaa59117a5045368019b488ec
MoritzLucaSchmid <luca.moritz.schmid@gmail.com>
2018-03-19 13:41:54 +0100

    MOT: Add MSC Data group types to header file

---
+++ lib/MOT.h
@@ -0,0 +1,30 @@
+/* -*- c++ -*- */
+/*
+ * Copyright 2018 Moritz Luca Schmid, Communications Engineering Lab (CEL)
+ * Karlsruhe Institute of Technology (KIT).
+ *
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifndef _MOT_H
+#define _MOT_H
+
+#define MSC_DATA_GROUP_TYPE_HEADER 3
+#define MSC_DATA_GROUP_TYPE_BODY 4
+#define MSC_DATA_GROUP_TYPE_DIR_UNCOMPRESSED 6
+#define MSC_DATA_GROUP_TYPE_DIR_COMPRESSED 7
+
+#endif /* _MOT_H */
